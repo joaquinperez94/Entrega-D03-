bb2ac3272267e28ae149ec61674a415dabcde3c3
espisepi <sepinaco@gmail.com>
2017-10-21 14:32:42 +0200

    Revert "Merge branch 'master' of https://github.com/joaquinperez94/Entrega-D03-"
    This reverts commit c2ccac133e2e592a5d595b734c55f82f570b6f12, reversing
    changes made to 66edecb99feabd0d620930f2ce22aed957439c7a.

--- Proyecto Java/Acme-Explorer/src/main/java/domain/Tag.java
+++ Proyecto Java/Acme-Explorer/src/main/java/domain/Tag.java
@@ -5,11 +5,13 @@ import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -35,6 +37,8 @@ public class Tag extends DomainEntity {
 
 
 	@ManyToMany(mappedBy = "tags")
+	@ElementCollection
+	@NotEmpty
 	public Collection<Trip> getTrips() {
 		return this.trips;
 	}
