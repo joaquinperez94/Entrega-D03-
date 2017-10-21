14a40e1ae63fa0e886e6adcdb948787040eee52c
MariaRuizGutierrez <32301062+MariaRuizGutierrez@users.noreply.github.com>
2017-10-21 11:36:40 +0200

    Añadido unas anotaciones en la clase endorserRecord.

--- Proyecto Java/Acme-Explorer/src/main/java/domain/EndorserRecord.java
+++ Proyecto Java/Acme-Explorer/src/main/java/domain/EndorserRecord.java
@@ -5,12 +5,14 @@ import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.URL;
 
 @Entity
@@ -55,6 +57,8 @@ public class EndorserRecord extends DomainEntity {
 		this.linkedProfile = linkedProfile;
 	}
 
+	@ElementCollection
+	@NotEmpty
 	public Collection<String> getComments() {
 		return this.comments;
 	}
