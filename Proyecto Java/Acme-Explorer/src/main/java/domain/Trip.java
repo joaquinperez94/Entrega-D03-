0a5378aab7e62dbceb829791aaf92bd4a7bda9b7
migueltern <32301597+migueltern@users.noreply.github.com>
2017-10-30 15:42:28 +0100

    Cambios sin A+

--- Proyecto Java/Acme-Explorer/src/main/java/domain/Trip.java
+++ Proyecto Java/Acme-Explorer/src/main/java/domain/Trip.java
@@ -19,18 +19,11 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import org.hibernate.search.annotations.Analyze;
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Index;
-import org.hibernate.search.annotations.Indexed;
-import org.hibernate.search.annotations.Store;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Access(AccessType.PROPERTY)
-@Indexed
-//L03A+
 public class Trip extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
@@ -49,7 +42,6 @@ public class Trip extends DomainEntity {
 	@NotBlank
 	@Column(unique = true)
 	@Pattern(regexp = "^[0-9]{2}(0[1-9]{1}|1[0-2]{1})((0|1|2)[0-9]{1}|3[0-1]{1})\\-[A-Z]{4}$")
-	@Field(index = Index.YES, analyze = Analyze.NO, store = Store.YES)
 	public String getTicker() {
 		return this.ticker;
 	}
@@ -59,7 +51,6 @@ public class Trip extends DomainEntity {
 	}
 
 	@NotBlank
-	@Field(index = Index.YES, analyze = Analyze.NO, store = Store.YES)
 	public String getTitle() {
 		return this.title;
 	}
@@ -69,7 +60,6 @@ public class Trip extends DomainEntity {
 	}
 
 	@NotBlank
-	@Field(index = Index.YES, analyze = Analyze.NO, store = Store.YES)
 	public String getDescription() {
 		return this.description;
 	}
